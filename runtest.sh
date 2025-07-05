e3d7458a32f7037d6ea0fe7eede2a02a229d89d9
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-07-05 15:25:15 -0500

    Formatting output.

--- runtest.sh
+++ runtest.sh
@@ -21,16 +21,16 @@ i=0
 while [ $i -lt 3600 ]; do
   sleep 1
 
-  echo "$(date) i = $i"
-
   RSS_KB=$(ps -eo pid,user,rss,time -q $PID | tail -1 | awk '{print $3}' )
-  echo "RSS_KB=$RSS_KB"
 
   RSS_MB=$(bc <<< "scale=1; $RSS_KB / 1000")
-  echo "RSS_MB=$RSS_MB"
 
   CPU_TIME=$(ps -eo pid,user,rss,time -q $PID | tail -1 | awk '{print $4}' )
-  echo "CPU_TIME=$CPU_TIME"
+
+  NUM_THREADS=$(ps -eLf -q $API_PID | grep -v PID | wc -l)
+
+  echo
+  echo "$(date) i=$i RSS_KB=$RSS_KB RSS_MB=$RSS_MB CPU_TIME=$CPU_TIME NUM_THREADS=$NUM_THREADS"
   
   i=$((i+1))
 done
