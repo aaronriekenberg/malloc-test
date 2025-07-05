86c17230013f9efce9a31fe4f04a5f5f160ece30
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-07-05 15:18:19 -0500

    Add code.

---
+++ runtest.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "begin runtest.sh"
+
+echo "cc --version"
+cc --version
+
+echo "cc -pthread test.c"
+cc -pthread test.c
+
+echo "run ./a.out"
+./a.out &
+
+PID=$!
+sleep 1
+
+i=0
+
+while [ i < 3600 ]; do
+  sleep 1
+
+  echo "$(date) i = $i"
+
+  RSS_KB=$(ps -eo pid,user,rss,time -q $API_PID | tail -1 | awk '{print $3}' )
+  echo "RSS_KB=$RSS_KB"
+
+  RSS_MB=$(bc <<< "scale=1; $RSS_KB / 1000")
+  echo "RSS_MB=$RSS_MB"
+
+  CPU_TIME=$(ps -eo pid,user,rss,time -q $API_PID | tail -1 | awk '{print $4}' )
+  echo "CPU_TIME=$CPU_TIME"
+  
+  i=$((i+1))
+done
+
+
+echo "end runtest.sh"
---
+++ test.c
@@ -0,0 +1,68 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#define NUM_THREADS (200)
+#define MAX_ALLOCATION_SIZE_BYTES (100 * 1024)
+
+void* runThread(void* param) {
+  int i;
+  int pageNumber;
+  void* pData;
+
+  while (true) {
+    size_t bytes = lrand48() % MAX_ALLOCATION_SIZE_BYTES;
+    if (bytes == 0) {
+       bytes = 1;
+    }
+    pData = malloc(bytes);
+    if (pData == NULL) {
+        printf("malloc returned NULL\n");
+        abort();
+    }
+    for (pageNumber = 0;
+         pageNumber < bytes;
+         pageNumber += 4096) {
+      *((unsigned char*)pData + pageNumber) = 0;
+    }
+    /*memset(pData, 0, ALLOCATION_SIZE_BYTES);*/
+    free(pData);
+  }
+
+  return NULL;
+}
+
+int main(int argc, char** arg) {
+  int i;
+  int retVal;
+  pthread_t* threadIDs;
+
+  printf("begin main\n");
+
+  printf("creating %d threads\n", NUM_THREADS);
+
+  threadIDs = calloc(NUM_THREADS, sizeof(pthread_t));
+
+  for (i = 0; i < NUM_THREADS; ++i) {
+    retVal = pthread_create(&threadIDs[i], NULL, &runThread, NULL);
+    if (retVal != 0) {
+      printf("pthread_create returned NULL\n");
+      abort();
+    }
+  }
+
+  printf("joining %d threads\n", NUM_THREADS);
+
+  for (i = 0; i < NUM_THREADS; ++i) {
+    retVal = pthread_join(threadIDs[i], NULL);
+    if (retVal != 0) {
+      printf("pthread_join returned NULL\n");
+      abort();
+    }
+  }
+
+  printf("end main\n");
+  return 0;
+}
